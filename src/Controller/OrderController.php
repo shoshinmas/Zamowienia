99f232efb1ad9e8b2841516cfe3788912a5486db
shoshinmas <shoshinmas@protonmail.com>
2022-06-24 15:41:35 +0200

    Form submits data to entity!

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use App\Entity\Order;
 use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -17,9 +19,10 @@ use Symfony\Component\VarDumper\VarDumper;
 class OrderController extends AbstractController
 {
     #[Route('/order', name: 'app_order')]
-    public function addToOrder(ManagerRegistry $doctrine, Request $request): Response
+    public function addToOrder(ManagerRegistry $doctrine, Request $request, EntityManagerInterface $em): Response
     {
         $orderedProductId = (int) ($request->query->get('id'));
+        $pPrice = 1;
         $order = new Order();
         $order->addProduct($doctrine->getRepository(Product::class)->find($orderedProductId));
         $form = $this->createFormBuilder($order)
@@ -28,8 +31,18 @@ class OrderController extends AbstractController
             ->add('makeOrder', SubmitType::class, ['label' => 'Make Order'])
             ->getForm();
         $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $order->setStatus("Pending");
+            $order->setTotalSum((int)('quantity'));
+            //$order->setClientEmail($data['clientEmail']);
+            $em->persist($order);
+            $em->flush();
+            //return $this->redirectToRoute('app_product');
+        }
         return $this->render('order/index.html.twig', [
             'form'=> $form->createView(),
+            'order'=> $order
         ]);
     }
 }
