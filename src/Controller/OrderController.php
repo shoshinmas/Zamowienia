5c1e3c4b3b88a2683de3958d4a6d26e3704e888c
shoshinmas <shoshinmas@protonmail.com>
2022-06-24 14:07:22 +0200

    Form corrected, route from product to order is valid. The order takes proper values of the product. Form has to be fixed and submit has to pass the data to order entity.

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -3,19 +3,33 @@
 namespace App\Controller;
 
 use App\Entity\Order;
-use App\Repository\ProductRepository;
+use App\Entity\Product;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\VarDumper\VarDumper;
 
 class OrderController extends AbstractController
 {
     #[Route('/order', name: 'app_order')]
-    public function index(ProductRepository $productRepository, Order $order): Response
+    public function addToOrder(ManagerRegistry $doctrine, Request $request): Response
     {
-        return $this->render('order/index.html.twig', [
-            'controller_name' => 'OrderController',
-            'products' => $order->getClientEmail(),
+        $orderedProductId = (int) ($request->query->get('id'));
+        $order = new Order();
+        $order->setClientEmail('clientEmail');
+        $order->addProduct($doctrine->getRepository(Product::class)->find($orderedProductId));
+        $order = $this->createFormBuilder($order)
+            ->add('clientEmail', TextType::class)
+            ->add('quantity', IntegerType::class)
+            ->add('makeOrder', SubmitType::class, ['label' => 'Make Order'])
+            ->getForm();
+        return $this->renderForm('order/index.html.twig', [
+            'order'=> $order
         ]);
     }
 }
--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,10 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Order;
 use App\Entity\Product;
 use App\Form\AddToCartType;
 use App\Repository\ProductRepository;
+use phpDocumentor\Reflection\Types\Integer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\BrowserKit\Request;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,14 +16,14 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ProductController extends AbstractController
 {
-    #[Route('/', name: 'app_product')]
+
+
+    #[Route(path: '/', name: 'app_product')]
     public function index(ProductRepository $productRepository): Response
     {
-        $form = $this->createForm(AddToCartType::class);
         return $this->render('product/index.html.twig', [
             'controller_name' => 'ProductController',
             'products' => $productRepository->findAll(),
-            'form' => $form->createView(),
         ]);
     }
 }
