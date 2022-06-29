96f53fd43c9d697654017e97bd210be52f249e40
shoshinmas <shoshinmas@protonmail.com>
2022-06-29 15:33:42 +0200

    Added admin's view on orders

--- src/Controller/Admin/OrderController.php
+++ src/Controller/Admin/OrderController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller\Admin;
 
+use App\Repository\OrderRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,12 +12,18 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class OrderController extends AbstractController
 {
-    #[IsGranted('ROLE_USER')]
+
+    public function __construct(
+        private readonly OrderRepository $orderRepository)
+    {
+
+    }
+   // #[IsGranted('ROLE_USER')]
     #[Route('/admin/order', name: 'admin_order')]
     public function index(): Response
     {
-        return $this->render('admin/order/index.html.twig', [
-            'controller_name' => 'OrderController',
+        return $this->render('admin/dashboard/index.html.twig', [
+            'orders' => $this->orderRepository->findAll(),
         ]);
     }
 }
