2ca7dc23749932f553724100873c048df77d11e8
shoshinmas <shoshinmas@protonmail.com>
2022-06-30 16:43:22 +0200

    Email sends out while ordering

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -11,17 +11,28 @@ use Ramsey\Uuid\Uuid;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bridge\Twig\Mime\NotificationEmail;
+use Symfony\Component\Mailer\MailerInterface;
 
 class OrderController extends AbstractController
 {
-    public function __construct(private readonly OrderRepository $orderRepository)
+    private $mailer;
+
+    public function __construct(private readonly OrderRepository $orderRepository, MailerInterface $mailer)
     {
+        $this->mailer = $mailer;
     }
 
     #[Route('/order/{externalId}', name: 'app_order')]
     public function showOrder(string $externalId): Response
     {
         $uuidExternalId = Uuid::fromString($externalId);
+        $this->mailer->send((new NotificationEmail())
+        ->subject('New order')
+        ->htmlTemplate('emails/order_notification.html.twig')
+        ->from('from@example.com')
+        ->to($this->orderRepository->findOneByUuid($uuidExternalId)->getClientEmail())
+        ->context(['order'=>$this->orderRepository->findOneByUuid($uuidExternalId)]));
         return $this->render('order/index.html.twig', [
             'order' => $this->orderRepository->findOneByUuid($uuidExternalId),
         ]);
